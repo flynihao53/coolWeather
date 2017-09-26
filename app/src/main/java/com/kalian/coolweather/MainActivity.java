ecd077af59f0de70cb40aa0ca74687d26d1a3bde
Your Name <you@example.com>
2017-09-26 13:57:37 +0800

    first commit

---
+++ app/src/main/java/com/kalian/coolweather/MainActivity.java
@@ -0,0 +1,19 @@
+package com.kalian.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iflytek.cloud.IdentityListener;
+import com.iflytek.cloud.IdentityResult;
+import com.iflytek.cloud.SpeechError;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/kalian/coolweather/SurfaceHolderCallBack.java
@@ -0,0 +1,10 @@
+package com.kalian.coolweather;
+
+/**
+ * Created by Administrator on 2017/9/25.
+ */
+
+public interface SurfaceHolderCallBack {
+        void closeCamera();
+        void openCamera();
+}
